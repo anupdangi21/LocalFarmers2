0f311619877f6b5c80da5edb1ad15cb0ee9d66bd
Abrian Bajracharya <np03cs4a220089@heraldcollege.edu.np>
2024-05-11 11:54:49 +0545

    Bug fixed on logout

--- customer/customer.js
+++ customer/customer.js
@@ -1,38 +1,8 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const loginForm = document.querySelector('.loginform form');
-//     loginForm.addEventListener('submit', async (event) => {
-//         event.preventDefault();
-//         const formData = new FormData(loginForm);
-//         const username = formData.get('username');
-//         const password = formData.get('password');
 
-//         try {
-//             const response = await fetch('/submit', {
-//                 method: 'POST',
-//                 headers: {
-//                     'Content-Type': 'application/json'
-//                 },
-//                 body: JSON.stringify({
-//                     username,
-//                     password
-//                 })
-//             });
-
-//             if (response.ok) {
-//                 window.location.href = response.url; // Redirect if login is successful
-//             } else {
-//                 const errorMessage = await response.text(); // Extract error message
-//                 alert(errorMessage); // Display error message in alert dialog box
-//             }
-//         } catch (error) {
-//             console.error('Error:', error);
-//             alert('An error occurred. Please try again later.');
-//         }
-//     });
-// });
 
 let subMenu = document.getElementById("subMenu");
 
 function toggleMenu(){
     subMenu.classList.toggle("open-menu");
 }
+
